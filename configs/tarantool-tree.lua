aef2a21c99ed2d2ae8868aab8061f9d6ac7d82df
bigbes <bigbes@gmail.com>
2016-04-08 15:26:54 +0300

    Update configuration files

--- configs/tarantool-tree.lua
+++ configs/tarantool-tree.lua
@@ -1,11 +1,20 @@
+local log = require('log')
+log.info("Preloading")
+
 box.cfg {
    listen            = '3310',
-   slab_alloc_arena  = 0.2,
+   slab_alloc_arena  = 8,
    log_level         = 5,
    logger_nonblock   = true,
    wal_mode          = "none",
 }
 
-local spc = box.schema.space.create("ycsb", { id = 1024 })
-spc:create_index('primary', { type = 'tree', parts = {1, 'STR'} } )
-box.schema.user.grant('guest', 'read,write,execute', 'universe')
+log.info("Creating space")
+box.once("space-prepare", function()
+	local spc = box.schema.space.create("ycsb", { id = 1024 })
+	spc:create_index('primary', { type = 'tree', parts = {1, 'STR'} } )
+	box.schema.user.grant('guest', 'read,write,execute', 'universe')
+end)
+log.info("Done")
+
+require('console').listen('var/tarantool-tree.control')
